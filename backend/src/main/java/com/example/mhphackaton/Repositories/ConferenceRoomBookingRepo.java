d298c20cb31e5c4c158ee33027f1c616c006397d
edi <draghici.edi@gmail.com>
2024-03-16 15:45:34 +0200

    Added ConferenceRoom and ConferenceRoomBooking Entity, Repo and Service TODO Service for both

--- backend/src/main/java/com/example/mhphackaton/Repositories/ConferenceRoomBookingRepo.java
+++ backend/src/main/java/com/example/mhphackaton/Repositories/ConferenceRoomBookingRepo.java
@@ -1,6 +1,11 @@
 package com.example.mhphackaton.Repositories;
 
+import com.example.mhphackaton.Entities.ConferenceRoomBooking;
+import com.example.mhphackaton.UserAuthentication.LoginMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConferenceRoomBookingRepo extends JpaRepository <ConferenceRoomBookingRepo, Integer> {
+import java.time.LocalDate;
+
+public interface ConferenceRoomBookingRepo extends JpaRepository <ConferenceRoomBooking, Integer> {
+    LoginMessage bookConferenceRoom(int userId, LocalDate date);
 }
