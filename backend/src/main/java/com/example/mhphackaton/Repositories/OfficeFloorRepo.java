c5266ab1b45538b78ff907490de4edcbecd982f9
tudorrw <143077299+tudorrw@users.noreply.github.com>
2024-03-16 13:26:02 +0200

    add packages

---
+++ backend/src/main/java/com/example/mhphackaton/Repositories/OfficeFloorRepo.java
@@ -0,0 +1,8 @@
+package com.example.mhphackaton.Repositories;
+
+import com.example.mhphackaton.Entities.OfficeFloor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfficeFloorRepo extends JpaRepository<OfficeFloor, Integer> {
+
+}
