de4cdf236b0223c4bd98713037446af157ae4368
tudorrw <143077299+tudorrw@users.noreply.github.com>
2024-03-16 12:20:02 +0200

    add some files

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1> da maaaa daaa merge est tareeee</h1>
+      <h1> da maaaa daaa merge esti tareeee</h1>
     </div>
   )
 }
